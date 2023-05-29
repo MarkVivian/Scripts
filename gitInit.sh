19ab3fffa557e773cc98b86fe682396894db8658
MarkVivian <markviviantech@gmail.com>
2023-05-29 19:00:40 +0300

    update on the gitInit file

--- gitInit.sh
+++ gitInit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 read -p "enter the url for the repository: " repo
+
 git init
-git add .
+git add README.md
+git commit -m "first commit"
+git branch -M main
 git remote add origin $repo
 git push -u origin main
