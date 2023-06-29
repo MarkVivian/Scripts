557bd54fc31cf8c5ce0549f662f6857c88513eff
MarkVivian <markviviantech@gmail.com>
2023-06-29 17:33:43 +0300

    added some new changes to the iso image script by changing the fuse to libfuse2

--- isoImageFix.sh
+++ isoImageFix.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # this is used when the iso images refuse to open..
-sudo apt-get install fuse
-
-sudo modprobe fuse
+sudo apt-get install libfuse2
+sudo apt-get update
+sudo apt-get upgrade -y
 
