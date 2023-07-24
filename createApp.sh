57a976c3a3c2daf5562896d94d0d3f9a7186937c
MarkVivian <markviviantech@gmail.com>
2023-07-24 15:19:13 +0300

    created a script to extract tar.gz, .xz, zip and deb

--- createApp.sh
+++ createApp.sh
@@ -3,16 +3,20 @@ echo "do you wish to: "
 echo "1) Create a React App"
 echo "2) Create a Next App"
 echo "3) Create a Node js App"
-read -p "Enter your choice 1 , 2 or 3:  " choice
+echo "4) create a rust app"
+read -p "Enter your choice 1, 2, 3, 4:  " choice
 
 if [[ $choice -eq 1 ]]; then
     npm create vite@latest
     # Add your desired actions for choice 1 here
 elif [[ $choice -eq 2 ]]; then
-    npx create-next-app
+    npx create-next-app@latest
 elif [[ $choice -eq 3 ]]; then
     npm init
+elif [[ $choice -eq 4 ]]; then
+	read -p "enter the project name: " value
+	cargo new $value
 else
-    echo "Invalid choice. Please enter either 1, 2 or 3."
+    echo "Invalid choice. Please enter either 1, 2, 3 or 4."
 fi
 
---
+++ extractingFiles.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "what kind of file are you extracting: "
+echo "1) tar.xz"
+echo "2) tar.gz"
+echo "3) .zip"
+echo "4) install .deb file"
+read -p "choose one of the following: " type
+read -p "what is the file name: " filename
+
+if [[ $type -eq 4 ]]; then
+	echo "dpkg -i $filename"
+	dpkg -i $filename
+	break
+else
+	read -p "do you wish to change the directory where the file will be stored: (y or n)" choice
+fi
+
+if [[ $choice = "y" ]]; then
+	read -p "please pass the path to the file: eg /path/to/location" location
+else
+	location="."
+fi
+
+if [[ $type -eq 1 ]]; then
+		echo "tar -xf $filename -C $location"
+		tar -xf $filename -C $location
+	elif [[ type -eq 2 ]]; then
+		echo "tar -xzf $filename -C $location"
+		tar -xzf $filename -C $location
+	else
+		echo "unzip $filename -d $location"
+		unzip $filename -d $location
+fi
