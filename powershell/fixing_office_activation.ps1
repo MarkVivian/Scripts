45d9cb3e4d7685c1fb1aca3ee9737c82e9502b9c
Mark Vivian <markviviantech@gmail.com>
2024-06-07 20:49:24 +0300

    created a script to handle the activation failed error in ms office

---
+++ powershell/fixing_office_activation.ps1
@@ -0,0 +1,37 @@
+try {
+    # Define the file name to search for
+    $fileName = "OSPPREARM.EXE"
+
+    # Define the directories to search in
+    $directories = @("C:\Program Files", "C:\Program Files (x86)")
+
+    # Initialize a variable to store the file path if found
+    $filePath = $null
+
+    # Search the specified directories for the file
+    foreach ($dir in $directories) {
+        $searchResults = Get-ChildItem -Path $dir -Recurse -ErrorAction SilentlyContinue -Filter $fileName
+        if ($searchResults) {
+            $filePath = $searchResults.FullName
+            break
+        }
+    }
+
+    # Check if the file was found
+    if ($filePath) {
+        Write-Host "File found at: $filePath"
+
+        # Run the file 3 times as admin
+        for ($i = 1; $i -le 3; $i++) {
+            Write-Host "Running $fileName - Attempt $i"
+            Start-Process -FilePath $filePath -Verb RunAs
+        }
+    } else {
+        Write-Host "File not found in the specified directories"
+    }
+
+}
+finally {
+    # Reset the execution policy back to the original policy
+    # Set-ExecutionPolicy -Scope Process -ExecutionPolicy Restricted
+}
