1d32c75a7e0c047fd417c1ccb0ee15a7370cc7c6
Mark Vivian <markviviantech@gmail.com>
2024-07-16 17:49:37 +0300

    modified the git_pull.sh script to run after 3 minutes

--- bash/git_pull.sh
+++ bash/git_pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# it will sleep for 2 minutes.
-sleep 120 
+# it will sleep for 3 minutes.
+sleep 180 
 
 cd ~/Documents/cyber_security/ && git pull > ~/cyber_git_pull.txt 2>&1
 cd ~/Documents/Scripts/ && git pull > ~/Scripts_git_pull.txt 2>&1
