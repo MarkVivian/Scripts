8805e8ec797c69e992a869a4bba1d76f5ee2abae
Mark Vivian <markviviantech@gmail.com>
2026-02-16 21:26:02 +0300

    just needed to write down some notes

--- bash/git_pull.sh
+++ bash/git_pull.sh
@@ -25,3 +25,11 @@ while true; do
             sleep 10
         fi 
 done
+# difference between pulling and cloning:
+# pulling is used to update an existing repository, while cloning is used to create a new copy of a repository. Pulling fetches the changes from the remote repository and merges them with the local repository
+# cloning creates a new directory with the contents of the remote repository. Pulling is typically used when you have already cloned a repository and want to keep it up to date, while cloning is used when you want to create a new copy of a repository for the first time.
+# EG.
+# cloning from a ssh url:
+# git clone ssh://username@hostname:port/path/to/repository.git
+# cloning from a https url:
+# git clone https://hostname/path/to/repository.git
